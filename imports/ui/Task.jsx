f447948a39b7f65553fef63ccf1d973c861fb277
A Machalica <a.machalica@selleo.com>
2016-10-29 23:39:53 +0200

    Implements tasks checking and removal

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,8 +1,36 @@
 import React, {Component, PropTypes} from "react";
+import {Tasks} from "../api/tasks";
 
 export default class Task extends Component {
+  toggleChecked() {
+    Tasks.update(this.props.task._id, {
+      $set: {checked: !this.props.task.checked}
+    })
+  };
+
+  deleteTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
-    return(<li>{this.props.task.text}</li>);
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+
+    return (
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteTask.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          defaultChecked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
+    );
   }
 }
 
