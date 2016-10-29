2ec0c828d35f54caeba684d5a7a6a29f9ea7e9dd
A Machalica <a.machalica@selleo.com>
2016-10-29 23:25:33 +0200

    Implements tasks adding to the database.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,17 +1,34 @@
-import React, {Component} from "react";
+import React, {Component, PropTypes} from "react";
+import {createContainer} from "meteor/react-meteor-data";
+import {Tasks} from "../api/tasks";
 import Task from "./Task.jsx";
 
-export default class App extends Component {
-  getTasks() {
-    return [
-      {_id: 1, text: "This is task 1"},
-      {_id: 2, text: "This is task 2"},
-      {_id: 3, text: "This is task 3"},
-    ]
-  };
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      taskName: ""
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    Tasks.insert({
+      text: this.state.taskName,
+      createdAt: new Date()
+    });
+
+    this.setState({taskName: ""})
+  }
+
+  updateTaskName(event) {
+    this.setState({taskName: event.target.value})
+  }
 
   renderTasks() {
-    return this.getTasks().map(task => {
+    return this.props.tasks.map(task => {
       return <Task key={task._id} task={task}/>;
     })
   };
@@ -21,9 +38,28 @@ export default class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input
+              type="text"
+              value={this.state.taskName}
+              onChange={this.updateTaskName.bind(this)}
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
         <ul>{this.renderTasks()}</ul>
       </div>
     )
   }
 }
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired
+};
+
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+  };
+}, App);
