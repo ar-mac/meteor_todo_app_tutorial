41e63cae2cd2085d0b33e0aa1fa1ffbb53eee5c4
A Machalica <a.machalica@selleo.com>
2016-11-03 23:43:39 +0100

    Adds metods to operate on tasks collection

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,2 +1,36 @@
+import {Meteor} from "meteor/meteor";
 import {Mongo} from "meteor/mongo";
+import {check} from "meteor/check";
+
 export const Tasks = new Mongo.Collection("tasks");
+
+Meteor.methods({
+  "tasks.insert"(text) {
+    check(text, String);
+    if (!this.userId) throw new Meteor.Error("not-authorized");
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: this.userId,
+      username: Meteor.users.findOne(this.userId).username,
+    });
+  },
+  "tasks.remove"(taskId) {
+    check(taskId, String);
+    if (!this.userId) throw new Meteor.Error("not-authorized");
+
+    const taskToRemove = Meteor.tasks.findOne({owner: {$eq: this.userId}});
+    if (taskToRemove) {
+      Tasks.remove(taskId);
+    } else {
+      throw new Meteor.Error("not-found");
+    }
+  },
+  "tasks.setCompleted"(taskId, setCompleted) {
+    check(taskId, String);
+    check(setCompleted, Boolean);
+
+    Tasks.update(taskId, {$set: {completed: setCompleted}})
+  }
+});
